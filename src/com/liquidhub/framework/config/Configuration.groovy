26c389377fad302cebbbdda8d35c81045166dbd1
Rahul Mishra <ibx4502@ibx.com>
2015-06-15 16:06:56 -0400

    Changes to accomodate configuration model

--- src/com/liquidhub/framework/config/Configuration.groovy
+++ src/com/liquidhub/framework/config/Configuration.groovy
@@ -3,53 +3,91 @@ package com.liquidhub.framework.config
 import groovy.transform.ToString
 
 import com.liquidhub.framework.model.CoreJobConfig
+import com.liquidhub.framework.model.DeploymentJobConfig
 import com.liquidhub.framework.model.GitflowBranchingConfig
+import com.liquidhub.framework.model.RoleConfig
 
 @ToString(includeNames=true)
+/**
+ * Represents the job generation related configuration elements. 
+ * 
+ * @author Rahul Mishra, LiquidHub
+ *
+ */
 class Configuration {
 	
-	CoreJobConfig continuousIntegrationConfig
+	/*
+	 * Developer Note: All the property names below MUST correspond to the key in the corresponding configuration file.
+	 * 
+	 * Changes on both sides MUST be made in sync otherwise properties will not load correctly
+	 * 
+	 */
+
+	CoreJobConfig continuousIntegrationConfig, codeQualityConfig
 
 	GitflowBranchingConfig gitflowFeatureBranchConfig,gitflowReleaseBranchConfig,gitflowHotfixBranchConfig
+
+	DeploymentJobConfig deploymentConfig
+
+	RoleConfig roleConfig
+
+	private Map notificationConfig, viewConfig, buildConfig
 	
+	private List buildPipelinePreferences
+
 	def buildEnvProperties = [:] //A mix of system environment variables and build enviroment variables.
 
 	private ConfigurationLevel level
 
 	private static logger
-	
-	
+
+
 	def merge(Configuration thatConfig){
 
 		CoreJobConfig.logger = logger
 
-		//An array of all job related configuration
-		def jobKeyConfigs = [
+		//An array of all configuration settings, these values below MUST sync with field names above
+		[
+			'roleConfig',
+			'codeQualityConfig',
 			'continuousIntegrationConfig',
 			'gitflowFeatureBranchConfig',
 			'gitflowReleaseBranchConfig',
-			'gitflowHotfixBranchConfig'
-		]
+			'gitflowHotfixBranchConfig',
+			'deploymentConfig',
+			'notificationConfig',
+			'viewConfig',
+			'buildPipelinePreferences'
+		].each{
+			this[it] = merge(this[it], thatConfig[it])
 
-		jobKeyConfigs.each{key ->
-			logger.debug('Now processing '+key+ 'from configuration level as '+thatConfig.level)
-
-			def thisJobConfig = this[key],thatJobConfig = thatConfig[key]
-
-
-			/*
-			 * If the configuration against this key exists
-			 *   1. Check if the incoming configuration for the same key exists, If it does merge them
-			 *   2. if it doesn't, retain the original config
-			 *   3. If the incoming configuration for the key does not exist, switch to target no matter what it is 
-			 */
-			this[key] = thisJobConfig ? (thatJobConfig ? thisJobConfig.merge(thatJobConfig) : thisJobConfig) : thatJobConfig
-
-			logger.debug('Now processed '+key+ 'from configuration level as '+thatConfig.level)
-			logger.debug 'updated setting for this key is '+this[key]
+			logger.debug it +' @ '+ thatConfig.level+': '+this[it]
 		}
 
 	}
+
+	protected def merge(src, target){
+
+		/*
+		 * If the src exists
+		 *   1. Check if the target exists, If it does merge them. When it's a map push, when its an object 'expect' a merge API
+		 *   2. if it doesn't, retain the original config
+		 *   3. If the src does not exist, switch to target no matter what it is
+		 */
+
+		if(src){
+			if(target){
+				src instanceof Map ? src << target : src.merge(target)
+			}else{
+				src
+			}
+
+		}else{
+			target
+		}
+
+	}
+
 	
 
 }
