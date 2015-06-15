e0b8712f90db0759e705e1a4c258b09dff601d06
Rahul Mishra <ibx4502@ibx.com>
2015-06-15 16:05:51 -0400

    Added a file to represent project roles

---
+++ src/com/liquidhub/framework/model/RoleConfig.groovy
@@ -0,0 +1,25 @@
+package com.liquidhub.framework.model
+
+import groovy.transform.ToString
+
+@ToString(includeNames=true, includePackage=false)
+class RoleConfig {
+
+	def developerRole, releaseManagerRole,deploymentManagerRole, projectAdminRole, qaAdminRole
+
+
+	def merge(RoleConfig otherRoleConfig){
+		
+		if(!otherRoleConfig)return this
+		
+		[
+			'developerRole',
+			'releaseManagerRole',
+			'deploymentManagerRole',
+			'projectAdminRole',
+			'qaAdminRole'
+		].each{property ->
+			this[property] = otherRoleConfig[property] ?:  this[property]
+		}
+	}
+}
