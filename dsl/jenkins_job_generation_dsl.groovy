f6eb6ef977543d1435a24b94803b9e1df4b6d97c
Rahul Mishra <ibx4502@ibx.com>
2015-06-16 15:38:59 -0400

    base dsl

---
+++ dsl/jenkins_job_generation_dsl.groovy
@@ -0,0 +1,4 @@
+import com.liquidhub.framework.providers.jenkins.JenkinsJobGenerationPipeline
+
+JenkinsJobGenerationPipeline pipeline = new JenkinsJobGenerationPipeline();
+pipeline.fabricate(binding, this);
