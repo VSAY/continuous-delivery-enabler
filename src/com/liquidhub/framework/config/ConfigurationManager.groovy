1dfca87ba5a0cac8d2735bacc700db608f768b32
Rahul Mishra <ibx4502@ibx.com>
2015-06-16 15:35:32 -0400

    updated groovydoc

--- src/com/liquidhub/framework/config/ConfigurationManager.groovy
+++ src/com/liquidhub/framework/config/ConfigurationManager.groovy
@@ -2,6 +2,13 @@ package com.liquidhub.framework.config
 
 import com.liquidhub.framework.config.model.Configuration
 
+/**
+ * Loads the configuration required for the job generation framework. 
+ * 
+ * @author Rahul Mishra, LiquidHub
+ *
+ */
+
 interface ConfigurationManager {
 
 	Configuration loadConfigurationForRepositoryBranch(projectName, branchName)
