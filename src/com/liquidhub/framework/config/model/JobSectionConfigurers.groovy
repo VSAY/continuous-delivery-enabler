217f6e11aaf57913d08694ddd5b068c58a389a85
Rahul Mishra <ibx4502@ibx.com>
2015-06-19 15:37:37 -0400

    Added OS section configurer

--- src/com/liquidhub/framework/config/model/JobSectionConfigurers.groovy
+++ src/com/liquidhub/framework/config/model/JobSectionConfigurers.groovy
@@ -1,10 +1,12 @@
 package com.liquidhub.framework.config.model
 
+
 import com.liquidhub.framework.JobSectionConfigurer
 import com.liquidhub.framework.providers.jenkins.ExtendedEmailNotificationSectionConfigurer
 import com.liquidhub.framework.providers.jenkins.GenericSCMSectionConfigurer
 import com.liquidhub.framework.providers.jenkins.GenericSCMTriggerSectionConfigurer
 import com.liquidhub.framework.providers.jenkins.MavenSectionConfigurer
+import com.liquidhub.framework.providers.jenkins.OperatingSystemCommandConfigurer;
 
 
 /**
@@ -21,7 +23,8 @@ enum JobSectionConfigurers {
 	GENERIC_SCM_SECTION_CONFIGURER(new GenericSCMSectionConfigurer()),
 	MAVEN_SECTION_CONFIGURER(new MavenSectionConfigurer()),
 	GENERIC_SCM_TRIGGER_SECTION_CONFIGURER(new GenericSCMTriggerSectionConfigurer()),
-	EXTENDED_EMAIL_SECTION_CONFIGURER(new ExtendedEmailNotificationSectionConfigurer())
+	EXTENDED_EMAIL_SECTION_CONFIGURER(new ExtendedEmailNotificationSectionConfigurer()),
+	JENKINS_OS_COMMAND_CONFIGURER(new OperatingSystemCommandConfigurer())
 
 	JobSectionConfigurers(JobSectionConfigurer provider){
 		this.provider = provider
