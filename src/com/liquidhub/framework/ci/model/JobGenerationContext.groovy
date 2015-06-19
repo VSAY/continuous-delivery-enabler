0fb2f6748de953a2a63f1c8e2fb03760c5e9fe04
Rahul Mishra <ibx4502@ibx.com>
2015-06-18 23:30:05 -0400

    added view job support

--- src/com/liquidhub/framework/ci/model/JobGenerationContext.groovy
+++ src/com/liquidhub/framework/ci/model/JobGenerationContext.groovy
@@ -1,36 +1,65 @@
 package com.liquidhub.framework.ci.model
 
 import com.liquidhub.framework.JobSectionConfigurer
-import com.liquidhub.framework.ci.JobGeneratorFactory
+import com.liquidhub.framework.ci.BuildEnvironmentAwareTemplateEngine
+import com.liquidhub.framework.ci.JobFactory
+import com.liquidhub.framework.ci.job.JobViewSupport
 import com.liquidhub.framework.ci.logger.Logger
+import com.liquidhub.framework.config.JobGenerationWorkspaceUtils
+import com.liquidhub.framework.config.model.BuildConfig
 import com.liquidhub.framework.config.model.Configuration
 import com.liquidhub.framework.providers.jenkins.JenkinsWorkspaceUtils
 import com.liquidhub.framework.scm.model.SCMRepository
 
-
+/**
+ * Represents the context in which jobs are generated, by default most properties in the context are immutable after it has been created
+ * 
+ * A context is unique for every job generation run. 
+ * 
+ * @author Rahul Mishra,LiquidHub
+ *
+ */
 class JobGenerationContext {
 
-	JobGeneratorFactory jobFactory
+	final JobFactory jobFactory
 
-	JenkinsWorkspaceUtils workspaceUtils
+	final JobGenerationWorkspaceUtils workspaceUtils
 
-	Configuration configuration
+	final Configuration configuration
 
 	private static Logger logger
 
-	SCMRepository scmRepository
+	final SCMRepository scmRepository
+
+	final def repositoryName, defaultRegularEmailRecipients,defaultEscalationEmailRecipients,repositoryBranchName, jobSeederName
+	
+	final BuildEnvironmentAwareTemplateEngine templateEngine
+	
+	final BuildConfig buildToolConfig
+	
+	final JobViewSupport viewHelper
+
+	JobGenerationContext(JobFactory jobFactory, JenkinsWorkspaceUtils workspaceUtils,Configuration configuration,SCMRepository scmRepository, JobViewSupport jobViewSupport){
+
+		this.workspaceUtils = workspaceUtils
+		this.jobFactory = jobFactory
+		this.configuration = configuration
+		this.scmRepository = scmRepository
+		this.viewHelper = jobViewSupport
+
+		//Some additional common properties for easy access.
+		this.repositoryName = scmRepository?.repositorySlug
+		this.repositoryBranchName = scmRepository?.repoBranchName
+		this.defaultEscalationEmailRecipients = scmRepository.repositorySlug+configuration.notificationConfig.defaultEscalationEmailSuffix
+		this.defaultRegularEmailRecipients = scmRepository.repositorySlug+configuration.notificationConfig.defaultRecipientEmailSuffix
+		this.buildToolConfig = configuration.buildConfig
+		this.templateEngine = new BuildEnvironmentAwareTemplateEngine(this)
+		this.jobSeederName = configuration.jobSeederName
+	}
+
 
 	public def generateJob(name, jobConfig){
 		jobFactory.job(name, jobConfig)
-	}
-
-
-	public def projectName(){
-		scmRepository?.projectKey
-	}
-
-	public def buildToolConfig(){
-		configuration.buildConfig
 	}
 
 
@@ -38,17 +67,15 @@ class JobGenerationContext {
 		configuration.configurableJobSections.provider(providerKey)
 	}
 
-	def  getDefaultRegularEmailRecipients(){
-		scmRepository.repositorySlug+configuration.notificationConfig.defaultRecipientEmailSuffix
-	}
 
-	def getDefaultEscalationEmailRecipients(){
-			scmRepository.repositorySlug+configuration.notificationConfig.defaultEscalationEmailSuffix
+	def getVariable(SeedJobParameters jobParameter){
+		configuration.buildEnvProperties[jobParameter.bindingName]
 	}
 	
-	def variable(SeedJobParameters jobParameter){
-	   configuration.buildEnvProperties[jobParameter.bindingName]	
+	/**
+	 * @return true if the context generation is happening on a windows machine
+	 */
+	boolean isGeneratingOnWindows(){
+		
 	}
-	
-	
 }
