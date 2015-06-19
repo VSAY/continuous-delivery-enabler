e7459e4c3f790a7c911b4a7e2d9243ae6180664a
Rahul Mishra <ibx4502@ibx.com>
2015-06-19 15:40:23 -0400

    enhanced abstraction

--- src/com/liquidhub/framework/JobSectionConfigurer.groovy
+++ src/com/liquidhub/framework/JobSectionConfigurer.groovy
@@ -1,7 +1,7 @@
 package com.liquidhub.framework
 
+import com.liquidhub.framework.ci.model.JobGenerationContext
 import com.liquidhub.framework.config.model.JobConfig
-import com.liquidhub.framework.model.JobGenerationContext
 
 /**
  * The base contractual expectation from a configuration section provider. All provider implementations MUST be stateless
