301f5c9ff8c74713eb97c76d0e82d1a3e5b1c147
Rahul Mishra <ibx4502@ibx.com>
2015-06-19 02:24:31 -0400

    abstraction notion of a job factory

---
+++ src/com/liquidhub/framework/ci/JobFactory.groovy
@@ -0,0 +1,19 @@
+package com.liquidhub.framework.ci
+
+
+/**
+ * A factory method to generate job configurations. Implementations MUST bind to a specific CI tool and use the closure configuration
+ * to generate the job configuration. Implementations MUST not throw an error if they recieve configuration elements that are not understood.
+ * They MUST throw an error when they do not recieve an element which is mandatory for the underlying provider
+ * 
+ * 
+ * @author Rahul Mishra, LiquidHub
+ *
+ */
+interface JobFactory {
+	
+	def job(name, Closure jobConfig)
+	
+	
+
+}
