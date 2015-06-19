d9fca6eec036f3fefba85ff5913b7ffee15f79dc
Rahul Mishra <ibx4502@ibx.com>
2015-06-19 02:25:11 -0400

    improved abstraction

--- src/com/liquidhub/framework/ci/job/generator/JobGenerator.groovy
+++ src/com/liquidhub/framework/ci/job/generator/JobGenerator.groovy
@@ -1,9 +1,14 @@
 package com.liquidhub.framework.ci.job.generator;
 
-import com.liquidhub.framework.ci.JobGeneratorFactory
-import com.liquidhub.framework.model.JobGenerationContext
+import com.liquidhub.framework.ci.model.JobGenerationContext
 
 
+/**
+ * A base expectation from a job generator. All job implementations MUST be stateless
+ * 
+ * @author Rahul Mishra,LiquidHub
+ *
+ */
 
 public interface JobGenerator {
 	
