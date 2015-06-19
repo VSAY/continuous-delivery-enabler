2dec537ce30e54636ad6c92944fd13203fcdac7f
Rahul Mishra <ibx4502@ibx.com>
2015-06-19 15:40:09 -0400

    new file representing a scm listing request

--- src/com/liquidhub/framework/scm/model/SCMRemoteRefListingRequest.groovy
+++ src/com/liquidhub/framework/scm/model/SCMRemoteRefListingRequest.groovy
@@ -2,8 +2,8 @@ package com.liquidhub.framework.scm.model
 
 class SCMRemoteRefListingRequest {
 	
-	 def targetUrl, authorizedUserDigest,displayLabel,branchFilterText,tagFilterText
-	 int tagDisplayCount
+	 def targetUrl, authorizedUserDigest,displayLabel='id',branchFilterText,tagFilterText
+	 int tagDisplayCount = 0
 	
 	
 
