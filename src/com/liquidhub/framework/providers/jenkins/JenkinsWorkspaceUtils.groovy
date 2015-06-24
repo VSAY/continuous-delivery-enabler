5fb1515ae63eac3052b70dba053c5069fad26e94
Rahul Mishra <ibx4502@ibx.com>
2015-06-24 14:33:17 -0400

    added an api to determine operation system

--- src/com/liquidhub/framework/providers/jenkins/JenkinsWorkspaceUtils.groovy
+++ src/com/liquidhub/framework/providers/jenkins/JenkinsWorkspaceUtils.groovy
@@ -16,4 +16,13 @@ class JenkinsWorkspaceUtils implements JobGenerationWorkspaceUtils {
 		fileReaderImplementation.readFileFromWorkspace(filePath)
 	}
 
+
+	@Override
+	public Object isRunningOnWindows() {
+		windowsOS
+	}
+	
+	private final boolean windowsOS = (System.getenv()['OS']?.toLowerCase() =~ /.*windows.*/).matches() //If the OS belongs to the windows family
+	
+
 }
