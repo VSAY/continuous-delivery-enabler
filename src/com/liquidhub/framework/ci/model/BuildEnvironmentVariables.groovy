682a056b7ef1dda55a53ff4aed08b159e5f0ed7a
Rahul Mishra <rahul.mishra@ibx.com>
2015-06-26 12:44:45 -0400

    Added variables to refer git repository username and password

--- src/com/liquidhub/framework/ci/model/BuildEnvironmentVariables.groovy
+++ src/com/liquidhub/framework/ci/model/BuildEnvironmentVariables.groovy
@@ -4,6 +4,8 @@ package com.liquidhub.framework.ci.model
  * Helps bind tool specific build parameters to logical concepts thus providing an easier upgrade/transition path and abstracting client code
  * from tool specific nuances
  * 
+ * These variables MUST exist in the build environment for the framework to work correctly
+ * 
  * @author Rahul Mishra,LiquidHub
  *
  */
@@ -12,7 +14,8 @@ enum BuildEnvironmentVariables {
 	BUILD_STATUS('$BUILD_STATUS'),
 	PROJECT_NAME('$PROJECT_NAME'),
 	BUILD_NUMBER( '$BUILD_NUMBER'),
-
+	GIT_REPOSITORY_USERNAME('${gitRepositoryUserName}'),
+	GIT_REPOSITORY_PASSWORD('${gitRepositoryPassword}')
 
 	public def paramValue
 
