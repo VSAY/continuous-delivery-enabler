feed4a631e91df295e9a2e28f18203a465f45f4b
Rahul Mishra <rahul.mishra@ibx.com>
2015-06-26 11:46:52 -0400

    renamed file, merged capabilities

---
+++ src/com/liquidhub/framework/scm/BranchNamesListingProvider.groovy
@@ -0,0 +1,48 @@
+package com.liquidhub.framework.scm
+
+import com.liquidhub.framework.ci.EmbeddedScriptProvider
+import com.liquidhub.framework.scm.model.SCMRemoteRefListingRequest
+
+class BranchNamesListingProvider implements EmbeddedScriptProvider {
+
+
+	@Override
+	public String getScript(Map bindings) {
+
+		SCMRemoteRefListingRequest request = bindings['requestParam']
+
+		request.with{
+			bindVariablesInScript(targetUrl, authorizedUserDigest, branchFilterText, listFullRefNames)
+		}
+	}
+
+
+	protected bindVariablesInScript(gitRepoUrl, credentialsHash, branchFilterText, listFullRefNames){
+		if(!gitRepoUrl){
+			throw new RuntimeException('gitRepoUrl must be specified while configuring the job, its required for listing the remote git references')
+		}
+
+		if(!credentialsHash){
+			throw new RuntimeException('credentialsHash must be specified while configuring the job, its required for connecting to git for listing remote references')
+		}
+
+	
+		if(branchFilterText){
+			branchFilterText = "'"+branchFilterText+"'"
+		}
+		
+		def listingAPI = listFullRefNames ? 'listFullBranchNames' : 'listShortBranchNames'
+
+
+		"""
+           |import com.liquidhub.framework.git.SCMRepositoryExplorer as explorer
+		   |explorer.${listingAPI}('${gitRepoUrl}', '${credentialsHash}', ${branchFilterText}).join(',')
+        """.stripMargin()
+	}
+	
+	
+	public static void main(String[] args){
+		BranchNamesListingProvider provider = new BranchNamesListingProvider()
+		println provider.getScript(['requestParam':[targetUrl:'http://stash.ibx.com/scm/rca/roam-web.git',authorizedUserDigest: 'YWRtaW46YWRtaW4=', branchFilterText: 'release']])
+	}
+}
