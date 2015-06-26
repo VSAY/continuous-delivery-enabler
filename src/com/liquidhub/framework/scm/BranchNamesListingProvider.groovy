879531fc36da2d6e6872f5d7120fbad0d251d17e
Rahul Mishra <rahul.mishra@ibx.com>
2015-06-26 13:28:37 -0400

    short branch listing api was returning wrong name

--- src/com/liquidhub/framework/scm/BranchNamesListingProvider.groovy
+++ src/com/liquidhub/framework/scm/BranchNamesListingProvider.groovy
@@ -26,21 +26,27 @@ class BranchNamesListingProvider implements EmbeddedScriptProvider {
 			throw new RuntimeException('credentialsHash must be specified while configuring the job, its required for connecting to git for listing remote references')
 		}
 
-	
+
 		if(branchFilterText){
 			branchFilterText = "'"+branchFilterText+"'"
 		}
-		
-		def listingAPI = listFullRefNames ? 'listFullBranchNames' : 'listShortBranchNames'
 
 
-		"""
+		if(listFullRefNames) {//If the branch filter text is 'feature/' the full name listing for a feature 'f1' is refs/heads/feature/f1
+			"""
            |import com.liquidhub.framework.git.SCMRepositoryExplorer as explorer
-		   |explorer.${listingAPI}('${gitRepoUrl}', '${credentialsHash}', ${branchFilterText}).join(',')
+		   |explorer.listFullBranchNames('${gitRepoUrl}', '${credentialsHash}', ${branchFilterText}).join(',')
         """.stripMargin()
+		
+		}else{ //If the branch filter text is 'feature/' the short name listing for a feature 'f1' is 'f1'
+			"""
+           |import com.liquidhub.framework.git.SCMRepositoryExplorer as explorer
+		   |explorer.listShortBranchNames('${gitRepoUrl}', '${credentialsHash}', ${branchFilterText}).collect{it.substring(${branchFilterText}.length())}.join(',')
+        """.stripMargin()
+		}
 	}
-	
-	
+
+
 	public static void main(String[] args){
 		BranchNamesListingProvider provider = new BranchNamesListingProvider()
 		println provider.getScript(['requestParam':[targetUrl:'http://stash.ibx.com/scm/rca/roam-web.git',authorizedUserDigest: '', branchFilterText: 'release']])
