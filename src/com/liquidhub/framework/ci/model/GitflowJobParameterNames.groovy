09ee5283a28b83b0834cf54f295799d670530dd3
Rahul Mishra <ibx4502@ibx.com>
2015-06-29 21:44:01 -0400

    more parameters

--- src/com/liquidhub/framework/ci/model/GitflowJobParameterNames.groovy
+++ src/com/liquidhub/framework/ci/model/GitflowJobParameterNames.groovy
@@ -8,8 +8,9 @@ enum GitflowJobParameterNames {
 	RELEASE_FROM_BRANCH('releaseFromBranch'),
 	RELEASE_BRANCH('releaseBranch'),
 	RELEASE_VERSION('releaseVersion','Default version to use when starting the branch'),
+	MILESTONE_RELEASE_VERSION('milestoneReleaseVersion','The version which will be assigned to your milestone release.  If you do not see any options , your project has already had a final release'),
 	DEVELOPMENT_VERSION('developmentVersion', 'Default development version to use when finishing the branch'),
-	NEXT_MILESTONE_DEV_VERSION('nextMilestoneDevelopmentVersion'),
+	NEXT_MILESTONE_DEV_VERSION('nextMilestoneDevelopmentVersion','This is the next development version after you make this milestone release. You cannot edit this field, but if you think the value in this field is incorrect, please contact your administrator'),
 
 	
 	HOTFIX_BRANCH('hotfixBranch','The hotfix you intend to finish'),
@@ -24,7 +25,7 @@ enum GitflowJobParameterNames {
 	ALLOW_SNAPSHOTS_WHILE_FINISHING_HOTFIX('allowSnapshots','Should we proceed with finishing the hotfix branch even if there are snapshot depenendencies? '),
 	PUSH_RELEASES('pushReleases','Push release branches to the remote upstream'),
 	RELEASE_BRANCH_VERSION_SUFFIX('releaseBranchVersionSuffix','Suffix to append to versions on the release branch.'),
-	ENABLE_FEATURE_VERSIONS('enableFeatureVersions', 'Append the feature name to the maven pom version on the feature branch.'),
+	ENABLE_FEATURE_VERSIONS('enableFeatureVersions', 'Append the feature name to the maven pom version on the feature branch. If your feature name is callidusSSOIntegration, your  maven pom version will include callidus_sso_integration. Notice the pattern and name your feature accordingly'),
 	PUSH_FEATURES('pushFeatures', 'Push the feature branch to the remote upstream once it is created'),
 	PUSH_HOTFIXES('pushHotfixes', 'Push this hotfix branch to repository'),
 	KEEP_FEATURE_BRANCH('keepBranch', 'Keep the feature branch after finishing the feature/merging it to develop'),
@@ -32,9 +33,10 @@ enum GitflowJobParameterNames {
 	KEEP_HOTFIX_BRANCH('keepBranch', 'Keep the hotfix branch after finishing the hotfix. You MUST delete it manually before you can create the next hotfix branch'),
 	SKIP_FEATURE_MERGE_TO_DEVELOP('noFeatureMerge','Turn off merging changes from this feature branch to develop'),
 	SQUASH_COMMITS('squashCommits','Squash all commits made on this branch into a single commit before merge'),
-	SKIP_RELEASE_BRANCH_MERGE('noReleaseMerge', 'Turn off merging changes from the release branch to master and develop'),
+	SKIP_RELEASE_BRANCH_MERGE('noReleaseMerge', 'Turn off merging changes from the release branch to master and develop. This value is used only when you are doing a non milestone (GA) release'),
 	SKIP_RELEASE_TAGGING('noTag', 'Turn off tagging the release in git'),
 	RELEASE_TAG_MESSAGE('tagMessage','Commit message to use when tagging the release.'),
+	MILESTONE_TAG_MESSAGE('milestoneTagMessage','Commit message to use when tagging the milestone.'),
 	NO_DEPLOY('noDeploy','Turn off maven deployment. If false the "deploy" goal is called. If true the "install" goal is called'),
 	SKIP_HOTFIX_TAGGING('noTag', 'Turn off tagging the hotfix in git'),
 	
