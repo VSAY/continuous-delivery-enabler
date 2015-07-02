60f8e78c18b52aa1df860eef28b1e499344018cd
Rahul Mishra <ibx4502@ibx.com>
2015-07-02 16:18:29 -0400

    added new parameter

--- src/com/liquidhub/framework/ci/model/DeploymentJobParameters.groovy
+++ src/com/liquidhub/framework/ci/model/DeploymentJobParameters.groovy
@@ -16,10 +16,11 @@ enum DeploymentJobParameters {
 	ARTIFACT_ID(new JobParameter(name: 'artifactId',description: 'The unique identifier of the artifact which needs to be deployed.',elementType: ViewElementTypes.READ_ONLY_TEXT)),
 	PACKAGING(new JobParameter(name:'packaging',description: 'The packaging of the artifact to be deployed.',elementType: ViewElementTypes.READ_ONLY_TEXT)),
 	ARTIFACT_VERSION(new JobParameter(name: 'artifactVersion',description: 'The version of the artifact which needs to be deployed',elementType: ViewElementTypes.SINGLE_SELECT_CHOICES)),
-	TARGET_CLUSTER_NAME(new JobParameter(name: 'targetClusterName', description: 'The Cluster/JVM Name for which the deployment is targeted.You do not have to change this value',elementType: ViewElementTypes.READ_ONLY_TEXT)),
-	TARGET_CELL_NAME(new JobParameter(name:'targetCellName',description: 'The cell for which the deployment is targeted.You do not have to change this value', elementType: ViewElementTypes.READ_ONLY_TEXT)),
-	APP_CONTEXT_ROOT(new JobParameter(name:'applicationContextRoot',description: 'The Web Application context root, you do not HAVE to change this value', elementType: ViewElementTypes.READ_ONLY_TEXT))
-
+	TARGET_CLUSTER_NAME(new JobParameter(name: 'targetClusterName', description: 'The Cluster/JVM Name for which the deployment is targeted.You do not have to change this value',elementType: ViewElementTypes.TEXT)),
+	TARGET_CELL_NAME(new JobParameter(name:'targetCellName',description: 'The cell for which the deployment is targeted.You do not have to change this value', elementType: ViewElementTypes.TEXT)),
+	APP_CONTEXT_ROOT(new JobParameter(name:'applicationContextRoot',description: 'The Web Application context root', elementType: ViewElementTypes.TEXT)),
+	REPLACEMENT_VERSION(new JobParameter(name:'replacementVersion',description: 'Sepcify the artifact version which this version should replace(if any)', elementType: ViewElementTypes.TEXT))
+	
 
 	public DeploymentJobParameters(parameter){
 		this.properties = parameter.properties
