0e297769ac08ca2d5af1638fa4c09099885c6e87
Rahul Mishra <ibx4502@ibx.com>
2015-07-02 15:58:17 -0400

    added name to the list view

--- src/com/liquidhub/framework/ci/view/generator/impl/BasicListViewGenerator.groovy
+++ src/com/liquidhub/framework/ci/view/generator/impl/BasicListViewGenerator.groovy
@@ -3,21 +3,30 @@ package com.liquidhub.framework.ci.view.generator.impl
 import com.liquidhub.framework.ci.model.JobGenerationContext
 import com.liquidhub.framework.ci.view.generator.ViewGenerator
 
+/**
+ * Creates a basic list view of all jobs for the repository
+ * 
+ * @author Rahul Mishra,LiquidHub
+ *
+ */
 
 class BasicListViewGenerator implements ViewGenerator{
 
 	@Override
 	public def generateView(JobGenerationContext ctx) {
-		ctx.generateView(ctx.repositoryName+'-view', 'listView', createView(ctx.repositoryName+'.*'))
+
+		def viewName = ctx.repositoryName+'-view'
+		def jobRegExp = ctx.repositoryName+'.*'
+
+		ctx.generateView(viewName, 'listView', createView(viewName, ))
 	}
 
 
 	public def createView(viewName, jobRegExp){
 
 		return 	{
-
+			name(viewName)
 			jobs { regex(jobRegExp) } //Our repository jobs are named by this convention
-
 			columns {
 				status()
 				weather()
