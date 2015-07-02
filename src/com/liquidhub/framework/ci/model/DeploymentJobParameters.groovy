5270c010e3e78d225d645b753ff078f4ca898f5f
Rahul Mishra <ibx4502@ibx.com>
2015-07-02 15:48:07 -0400

    deployment job parameters

---
+++ src/com/liquidhub/framework/ci/model/DeploymentJobParameters.groovy
@@ -0,0 +1,34 @@
+package com.liquidhub.framework.ci.model
+
+import static com.liquidhub.framework.ci.view.ViewElementTypes.TEXT
+
+import com.liquidhub.framework.ci.view.ViewElementTypes
+
+/**
+ * Collection of deployment job parameters
+ * 
+ * @author Rahul Mishra,LiquidHub
+ *
+ */
+enum DeploymentJobParameters {
+
+	GROUP_ID(new JobParameter(name: 'groupId',description: 'The group id of the artifact which needs to be deployed', elementType : ViewElementTypes.READ_ONLY_TEXT)),
+	ARTIFACT_ID(new JobParameter(name: 'artifactId',description: 'The unique identifier of the artifact which needs to be deployed.',elementType: ViewElementTypes.READ_ONLY_TEXT)),
+	PACKAGING(new JobParameter(name:'packaging',description: 'The packaging of the artifact to be deployed.',elementType: ViewElementTypes.READ_ONLY_TEXT)),
+	ARTIFACT_VERSION(new JobParameter(name: 'artifactVersion',description: 'The version of the artifact which needs to be deployed',elementType: ViewElementTypes.SINGLE_SELECT_CHOICES)),
+	TARGET_CLUSTER_NAME(new JobParameter(name: 'targetClusterName', description: 'The Cluster/JVM Name for which the deployment is targeted.You do not have to change this value',elementType: ViewElementTypes.READ_ONLY_TEXT)),
+	TARGET_CELL_NAME(new JobParameter(name:'targetCellName',description: 'The cell for which the deployment is targeted.You do not have to change this value', elementType: ViewElementTypes.READ_ONLY_TEXT)),
+	APP_CONTEXT_ROOT(new JobParameter(name:'applicationContextRoot',description: 'The Web Application context root, you do not HAVE to change this value', elementType: ViewElementTypes.READ_ONLY_TEXT))
+
+
+	public DeploymentJobParameters(parameter){
+		this.properties = parameter.properties
+	}
+
+	def properties
+	
+	
+	public static void main(String[] args){
+		println GROUP_ID.properties
+	}
+}
