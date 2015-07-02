1e332d18d5364d9727059470cf36519abe57eac0
Rahul Mishra <ibx4502@ibx.com>
2015-07-02 15:54:30 -0400

    overload api

--- src/com/liquidhub/framework/providers/jenkins/JenkinsJobFactory.groovy
+++ src/com/liquidhub/framework/providers/jenkins/JenkinsJobFactory.groovy
@@ -1,35 +1,53 @@
 package com.liquidhub.framework.providers.jenkins
 
-import groovy.lang.Closure;
-
 import com.liquidhub.framework.ci.JobFactory
 import com.liquidhub.framework.ci.view.JobViewFactory
+import com.liquidhub.framework.config.JobGenerationWorkspaceUtils
 
-class JenkinsJobFactory implements JobFactory,JobViewFactory {
+/**
+ * A Jenkins Job DSL implementation of all three facets of job generation, view generation and workspace utilities
+ * 
+ * @author Rahul Mishra,LiquidHub
+ * 
+ */
+class JenkinsJobFactory implements JobFactory,JobViewFactory,JobGenerationWorkspaceUtils {
 
 	private def jenkinsDSLFactory
 
 	JenkinsJobFactory(dslFactory){
 		this.jenkinsDSLFactory = dslFactory
 	}
-
-
-	@Override
-	def job(name, Closure jobConfig) {
-	    jenkinsDSLFactory.freeStyleJob(name, jobConfig)
-	}
 	
+	@Override
+	def job(name, type='freeStyleJob', Closure jobConfig) {
+		jenkinsDSLFactory."$type"(name, jobConfig)
+	}
+
 	@Override
 	def view(name, type, Closure viewConfig) {
 		this.jenkinsDSLFactory."${type}"(name, viewConfig)
 	}
-	
+
 	def getImpl(){
 		jenkinsDSLFactory
 	}
 
 
-	
-	
-	
+	@Override
+	public fileReader(filePath) {
+		jenkinsDSLFactory.readFileFromWorkspace(filePath)
+	}
+
+
+	@Override
+	public Object isRunningOnWindows() {
+		windowsOS
+	}
+
+	private final boolean windowsOS = (System.getenv()['OS']?.toLowerCase() =~ /.*windows.*/).matches() //If the OS belongs to the windows family
+
+
+
+
+
 }
