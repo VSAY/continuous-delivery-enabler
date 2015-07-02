79a49c7047debeb01217c5b0da08461fdbee5571
Rahul Mishra <ibx4502@ibx.com>
2015-07-02 15:52:16 -0400

    generic description of a job parameter

---
+++ src/com/liquidhub/framework/ci/model/JobParameter.groovy
@@ -0,0 +1,15 @@
+package com.liquidhub.framework.ci.model
+
+import com.liquidhub.framework.ci.view.ViewElementTypes;
+
+class JobParameter {
+
+    def name, description,defaultValue=''
+	
+	ViewElementTypes elementType
+	
+	ParameterListingScript valueListingScript
+	
+
+		
+}
