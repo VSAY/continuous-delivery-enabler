3d46a68bf13ef0f6be6b6ad975191f7370f30ffe
Rahul Mishra <ibx4502@ibx.com>
2015-07-06 23:55:02 -0400

    Isolated url patterns to be repository provider specific

--- src/com/liquidhub/framework/providers/stash/GitRepository.groovy
+++ src/com/liquidhub/framework/providers/stash/GitRepository.groovy
@@ -1,6 +1,8 @@
 package com.liquidhub.framework.providers.stash
 
 
+import groovy.text.SimpleTemplateEngine
+import groovy.text.TemplateEngine
 import groovy.transform.ToString
 
 import com.liquidhub.framework.ci.model.BuildEnvironmentVariables
@@ -13,14 +15,25 @@ class GitRepository implements SCMRepository {
 
 	private def repoUrl,baseUrl,repoBranchName,projectKey,repositorySlug,authorizedUserDigest,changeSetUrl,branchType,releasePushUrl
 	
+	private GitRepositoryProvider repositoryProvider
+
+	private TemplateEngine engine = new SimpleTemplateEngine()
+
 	GitRepository(Map bindingVariables){
 
 		def repoBranchName = bindingVariables[SeedJobParameters.REPO_BRANCH_NAME.bindingName]
 		def repoUrl = bindingVariables[SeedJobParameters.SCM_REPO_URL.bindingName]
 		def repositoryUserCredentials = bindingVariables[SeedJobParameters.REPO_ACCESS_CREDENTIALS.bindingName]
 		def repositoryType = bindingVariables[SeedJobParameters.REPO_IMPLEMENTATION.bindingName] ?: 'STASH'//By default, we assume its a stash repository
+        
 		
-		def gitRepoURLPattern = GitRepositories.valueOf(repositoryType.toUpperCase()).urlPattern
+		try{
+			this.repositoryProvider = GitRepositoryProvider.valueOf(repositoryType.toUpperCase())
+		}catch(IllegalArgumentException iae){
+			throw new RuntimeException('Unsupported git repository. Expected one of '+GitRepositoryProvider.values() +' is supported. Recieved '+repositoryProvider)
+		}
+
+		def gitRepoURLPattern = repositoryProvider.cloneUrlPattern
 
 		def matcher = ( repoUrl =~  gitRepoURLPattern )//Git URL Repo Pattern
 
@@ -41,22 +54,22 @@ class GitRepository implements SCMRepository {
 		this.repositorySlug = matcher[0][4]
 		this.authorizedUserDigest= repositoryUserCredentials.bytes.encodeBase64()
 		this.repoUrl = repoUrl
-		this.changeSetUrl = createChangeSetUrl(baseUrl, projectKey, repositorySlug)
+		this.changeSetUrl = createChangeSetUrl(repositoryProvider, [baseUrl: baseUrl,projectKey: projectKey,repositorySlug: repositorySlug])
 		this.branchType = GitFlowBranchTypes.type(repoBranchName)
-		
+
 		final def gitUserName = BuildEnvironmentVariables.GIT_REPOSITORY_USERNAME.paramValue
 		final def gitPassword =  BuildEnvironmentVariables.GIT_REPOSITORY_PASSWORD.paramValue
-		
-		releasePushUrl = createReleasePushUrl(gitUserName, gitPassword, serverHost, projectKey, repositorySlug)
-	
+
+		releasePushUrl = createReleasePushUrl(repositoryProvider, [gitUser:gitUserName,gitPassword: gitPassword,serverHost: serverHost,projectKey: projectKey,repositorySlug: repositorySlug])
+
 	}
 
-	protected def createChangeSetUrl(baseUrl, projectKey, repositorySlug){
-		"${baseUrl}/projects/${projectKey}/repos/${repositorySlug}"
+	protected def createChangeSetUrl(repositoryType, bindings){
+		engine.createTemplate(this.repositoryProvider.changeSetUrlPattern).make(bindings).toString()
 	}
-	
-	protected def createReleasePushUrl(gitUser, gitPassword, serverHost, projectKey, repositorySlug){
-		"http://${gitUser}:${gitPassword}@${serverHost}/scm/${projectKey}/${repositorySlug}.git"
+
+	protected def createReleasePushUrl(repositoryType, bindings){
+		engine.createTemplate(this.repositoryProvider.releasePushUrlPattern).make(bindings).toString()
 	}
 
 	@Override
@@ -93,41 +106,43 @@ class GitRepository implements SCMRepository {
 	public def getChangeSetUrl() {
 		this.changeSetUrl
 	}
-	
+
 	@Override
 	public def getBranchType() {
 		this.branchType
 	}
 
-	
+
 	@Override
 	public def getReleasePushUrl() {
 		this.releasePushUrl
 	}
 
 
-	private enum GitRepositories {
+	private enum GitRepositoryProvider {
 
 
-		STASH('(http://)(.*)/scm/([A-z0-9]+)/(.*).git')
-		
+		STASH('(http://)(.*)/scm/([A-z0-9]+)/(.*).git','http://${gitUser}:${gitPassword}@${serverHost}/scm/${projectKey}/${repositorySlug}.git', '${baseUrl}/projects/${projectKey}/repos/${repositorySlug}')
 
-		public GitRepositories(urlPattern){
-			this.urlPattern = urlPattern
+
+		public GitRepositoryProvider(cloneUrlPattern, releasePushUrlPattern, changeSetUrlPattern){
+			this.cloneUrlPattern = cloneUrlPattern
+			this.releasePushUrlPattern = releasePushUrlPattern
+			this.changeSetUrlPattern = changeSetUrlPattern
 		}
 
-		def urlPattern
+		def cloneUrlPattern,releasePushUrlPattern,changeSetUrlPattern
 
 
 	}
 
 
-	
+
 
 	static def void main(String[] args){
 		def x = new GitRepository(['gitRepoUrl':'http://stash.test.ibx.com/scm/mvn/parent-pom.git','repoBranchName':'develop','repositoryUserCredentials':'a:b']);
 		println x.toString()
 	}
-	
+
 
 }
