7a477102d243f079abf16641eabf86270fee6a98
Rahul Mishra <ibx4502@ibx.com>
2015-09-30 16:44:50 -0400

    dropped extra hyphens

--- resources/core/liferay-hot-deployment-script.sh
+++ resources/core/liferay-hot-deployment-script.sh
@@ -1,2 +1,2 @@
-ssh -i /opt/apps/jenkins/.ssh/id_rsa_jenkssh jenkssh@${deploymentHost} "sudo su - liferay -c /opt/apps/scripts/deploy.sh ----deployDir ${deployDir} --groupId ${groupId} --artifactId ${artifactId} --version ${artifactVersion}"
+ssh -i /opt/apps/jenkins/.ssh/id_rsa_jenkssh jenkssh@${deploymentHost} "sudo su - liferay -c /opt/apps/scripts/deploy.sh --deployDir ${deployDir} --groupId ${groupId} --artifactId ${artifactId} --version ${artifactVersion}"
 
